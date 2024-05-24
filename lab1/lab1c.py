9d7532583645c4a5d999ca686fc06b5e867546f7
Aashirbad Shahi <ashahi6@myseneca.ca>
2024-05-24 14:08:18 -0400

    lab1 done.

---
+++ lab1/lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define name and age objects
+name = "Isaac"
+age = 72
+
+# Print out the message combining name and age
+print(name + " is " + str(age) + " years old!")
+
+
+
+
